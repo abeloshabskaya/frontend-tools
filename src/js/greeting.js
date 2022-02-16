2b386dc9b5cbfe0391dd8654b1096baf0ab218bb
abeloshabskaya <71314836+abeloshabskaya@users.noreply.github.com>
2022-02-16 15:40:54 +0300

    Add files via upload

---
+++ src/js/greeting.js
@@ -0,0 +1,5 @@
+function greeting(a) {
+    console.log(a);
+}
+
+export default greeting
---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+console.log('Script loaded');
+
+import { message } from './data.js';
+import greeting from './greeting.js'; 
+
+greeting(message);
